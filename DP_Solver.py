d1b730d4c7466b076dbfbe19abe0dabc4c9a006d
SimonaLoGiudice <simonalogiudice6@hotmail.it>
2019-09-18 19:06:53 +0200

    unit_clauses tested and worked. studying how to update the clauses when splitting occours (beginning of the DP algorithm)

--- DP_Solver.py
+++ DP_Solver.py
@@ -5,6 +5,7 @@ What functions we need?
 """
 import DIMACS_reader
 from sympy import *
+import random
 
 location_sudoku = "sudoku-example (1).txt"
 location_rules = "sudoku-rules.txt"
@@ -13,25 +14,62 @@ location_rules = "sudoku-rules.txt"
 
 def DP_solver(clauses, literals):
     #check unit clauses
-    unit_clauses = [c for c in clauses if len(c) == 1]
+    for lit in literals.key() if lietrals[lit] != None:
+        if literals[lit] == True:
+            # delete true clauses and simplify the negatives
+        elif literals[lit] == False:
+            #delete false clauses and simplify the negatives
+
+    unit_clauses = [c[0] for c in clauses if len(c) == 1]
     if unit_clauses is not {}:
+        print(unit_clauses)
         for unit in unit_clauses:
             x = int(unit)
-            print ("unit clause= ", x)
+            print ("unit clause ", x)
             if x > 0:
                 literals[x] = True
-            else:
-                literals[x] = False
+                print(x, literals[x])
+            elif x < 0:
+                literals[-x] = False
+                print(-x, literals[-x])
             for clause in clauses:
                 if x in clause:
-                    print ("x contained in clause: ", clause, " --> removing clause")
+                    print (x, " contained in clause: ", clause, " --> removing clause")
                     clauses.remove(clause)
                 if -x in clause:
-                    print("-x contained in clause: ", clause, " --> removing -x from this clause")
-                    new_clause = clause.remove(-x)
+                    print( x,  "contained in clause: ", clause, " --> removing ", -x, " from this clause")
+                    new_clause = clause
+                    new_clause.remove(-x)
                     print("new clause: ", new_clause)
-                    clauses[clause] = new_clause
-    return True
+                    clauses.remove(clause)
+                    clauses.append(new_clause)
+                    if len(new_clause) == 1:
+                        print("adding another unit clause to unit_clauses", new_clause[0])
+                        unit_clauses.append(new_clause[0])
+    if clauses is []:
+        return True
+    if clauses is None:
+        return False
+    else:
+        #splitting
+        vars = [v for v in literals.keys() if literals[v] == None]
+        #print (vars)
+        #random assignment
+        l = random.choice(vars)
+        literals[l] = True
+
+        if DP_solver(clauses, literals):
+            return True
+        else:
+            literals[l] = False
+            if DP_solver(clauses, literals):
+                return True
+            else:
+                return False
+
+
+
+
 
 
 
@@ -87,6 +125,8 @@ def main():
     #print (size, literals)
     #check for tautologies just once at the beginning
     clauses = checkTaut(clauses, literals)
+    new_literals = DP_solver(clauses, literals)
+    #call the recursive DP
 
     #literals = {111:None, 112:None, 113:None, 114:None, 115:None, 116:None, 117:None, 118:None, 119:None, 121:None}
     #TODO: get the size of literals from DIMACS, and declaire the dict list of keys value (111, 112, ... 999) literals[x]='' take out all the variables containing 0
