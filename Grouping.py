683345f0273ab239e5a7972c108a44b13e3535d1
SimonaLoGiudice <simonalogiudice6@hotmail.it>
2019-09-26 17:56:18 +0200

    Right version

---
+++ Grouping.py
@@ -0,0 +1,19 @@
+from __future__ import print_function,division
+import DIMACS_reader
+
+
+location = ["TXT/1000 sudokus.txt", "TXT/top91.sdk.txt", "TXT/damnhard.sdk.txt", "TXT/top95.sdk.txt", "TXT/top100.sdk.txt"]
+
+def get_sudokus(location):
+    sudokus = []
+    for l in location:
+        with open (l) as file:
+            s = file.read ()
+            sudokus += s.split ('\n')
+
+    print (sudokus)
+    # for sudoku in sudokus:
+    return 1
+
+
+get_sudokus(location)
